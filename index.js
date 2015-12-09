66943cee689ed8922c06b97d504f010ffe221a67
Daichi Kimura <kimura@feedforce.jp>
2015-12-09 09:58:40 +0900

    Fixed ESLint Error

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var auth           = require('./lib/auth');
+var auth = require('./lib/auth');
 var requestHandler = require('./lib/requestHandler');
 
 exports.handler = function(event, context) {
-  var raw_data  = event.text.match(/(lgtm\:\s*)(.*)/)[2];
-  var command   = raw_data.split('+')[0];
-  var options   = raw_data.split('+').slice(1);
-  var token     = event.token;
+  var rawData = event.text.match(/(lgtm\:\s*)(.*)/)[2];
+  var command = rawData.split('+')[0];
+  var options = rawData.split('+').slice(1);
+  var token = event.token;
 
   // Authenticate by token
   if (auth(token)) {
-    console.log(requestHandler(command, options, context));
-    return requestHandler(command, options, context);
+    context.done(null, requestHandler(command, options, context));
   } else {
-    return context.done(null, {text: "Auth Error."});
+    context.done(null, {text: 'Auth Error.'});
   }
 };
