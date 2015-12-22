080f3f616c918b9730171f06d48a2376d7ec1239
otofu-square <yukiyakonkon1024@gmail.com>
2015-12-22 13:01:07 +0900

    fix ping.js(using lambda-local)

--- test/ping.js
+++ test/ping.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'hogehoge',
+  token: process.env.SLACK_OUTGOING_WEBHOCKS_TOKEN,
   team_id: 'T0001',
   team_domain: 'example',
   channel_id: 'C2147483705',
@@ -7,6 +7,6 @@ module.exports = {
   timestamp: '1355517523.000005',
   user_id: 'U2147483697',
   user_name: 'Steve',
-  text: 'lgtm: ping',
+  text: 'lgtm: list',
   trigger_word: 'googlebot:'
 }
